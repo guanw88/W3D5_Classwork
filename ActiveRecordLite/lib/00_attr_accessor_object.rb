a7277ea571184f4e518d4077a5a5a3954c2ba548
gwang <guanw88@gmail.com>
2018-11-09 11:27:36 -0800

    Create custom attr_accessor class method

--- ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,17 @@
+require 'byebug'
+
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) {
+        instance_variable_get("@#{name}")
+      }
+
+      name_with_equals = "#{name}=".to_sym
+
+      define_method(name_with_equals) { |value|
+        instance_variable_set("@#{name}", value)
+      }
+    end
   end
 end
